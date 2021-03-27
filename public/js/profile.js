2699d5fc155e76ce61656547242ed0b18d63e2d4
lauracejas <lauracejasmartin94@gmail.com>
2021-03-27 08:47:37 -0400

    profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,26 +10,8 @@ const searchBooks = async (event) => {
   });
   const data = await response.json();
   console.log(data);
-
-  renderBook(data);
 };
 
 
-
-// Add a book I have read to a mysql database
-// const addHaveRead = (event) => {
-//   event.preventDefault();
-//   const haveReadTable = [];
-//var searchInput = JSON.parse({ search: searchInput.value }) || [];  
-//};
-
-// Add a book I want read to a mysql database
-// const addWantRead = async (event) => {
-//   event.preventDefault();
-
-// };
-
-
 document.querySelector("#booksearch").addEventListener("click", searchBooks);
-document.querySelector("#btn-haveread").addEventListener("click", addHaveRead);
-document.querySelector("#btn-wantread").addEventListener("click", addWantRead);
+
